de87504ff085b8e25fe9983728567ea1210e9844
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-21 19:22:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Exclude these modules from the bundle. They are loaded via importmap in index.html.
+      external: ['jspdf', 'html2canvas']
+    }
+  }
 })
