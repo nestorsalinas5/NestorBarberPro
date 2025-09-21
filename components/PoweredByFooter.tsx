f2af05e5346f04d29c0f9471b1d79c101b98b12c
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-21 19:44:34 -0300

    Create PoweredByFooter.tsx

---
+++ components/PoweredByFooter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PoweredByFooter: React.FC = () => {
+  return (
+    <footer className="text-center mt-12 text-brand-text-secondary text-sm">
+      <p>Potenciado por NestorBarberPro</p>
+    </footer>
+  );
+};
