ff22ed8b3b59656b5a2543316ee74e786d4765a7
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-21 22:46:58 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'nestorbarberpro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  // Note: We don't cache index.tsx or other JS modules directly,
+  // as they are part of the build process. The browser caches them.
+  // We also don't cache the importmap resources as they're on a CDN.
+  // This basic SW is for the app shell.
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
