e139e57b6d176785263386c39ec1355b60546412
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-22 23:12:28 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 // El nombre del caché. Es buena práctica incluir una versión para facilitar las actualizaciones.
-const CACHE_NAME = 'nestorbarberpro-v7';
+const CACHE_NAME = 'nestorbarberpro-v8';
 
 // App Shell: Archivos básicos y estáticos necesarios para que la aplicación se ejecute.
 const APP_SHELL_URLS = [
   '/',
   '/index.css',
   '/manifest.json',
+  '/icon.svg'
 ];
 
 /**
