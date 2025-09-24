b6345fdb142827da359e39555e85d6895fac8007
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-23 22:04:26 -0300

    Update BarberShopListItem.tsx

--- components/BarberShopListItem.tsx
+++ components/BarberShopListItem.tsx
@@ -46,8 +46,7 @@ export const BarberShopListItem: React.FC<BarberShopListItemProps> = ({ barberSh
       <div className="flex-grow">
         <div className="flex items-center gap-4">
             <p className="font-bold text-lg text-brand-text">{barberShop.name}</p>
-            {/* FIX: Use color_primario consistent with the updated BarberShop type. */}
-            {barberShop.color_primario && <span className="h-4 w-4 rounded-full" style={{ backgroundColor: barberShop.color_primario }}></span>}
+            {barberShop.primary_color && <span className="h-4 w-4 rounded-full" style={{ backgroundColor: barberShop.primary_color }}></span>}
         </div>
         <div className="flex items-center gap-2 mt-1">
           <span className={`h-2 w-2 rounded-full ${indicator}`}></span>
