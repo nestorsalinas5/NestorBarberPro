d188755d71afa5368c64e373169a8b6da11c256c
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-26 14:59:21 -0300

    Update PoweredByFooter.tsx

--- components/PoweredByFooter.tsx
+++ components/PoweredByFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const PoweredByFooter: React.FC = () => {
   return (
-    <footer className="text-center mt-12 text-brand-text-secondary text-sm">
+    <footer className="text-center mt-12 text-brand-light-beige/70 text-sm">
       <p>Potenciado por NestorBarberPro</p>
     </footer>
   );
